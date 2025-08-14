e1174c27daf0ef5168b3603afca27b1775db1d0c
dmqfr <dmqfr@PANDA_PC>
2025-08-14 12:37:44 -0500

    Add project files.

---
+++ AZ_Services/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+using System.Diagnostics;
+using AZ_Services.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AZ_Services.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult ServiceAreas()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AZ_Services/Controllers/ServicesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AZ_Services.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Individual services
+        public IActionResult HVAC()
+        {
+            return View();
+        }
+
+        public IActionResult Plumbing()
+        {
+            return View();
+        }
+
+        public IActionResult Electrical()
+        {
+            return View();
+        }
+    }
+
+}
